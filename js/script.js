6836a9b2b6d8a20927155ec889376f3bf91a0069
AristidesJBN <84427194+AristidesJBN@users.noreply.github.com>
2023-07-19 22:03:55 -0300

    Identação código JS

--- js/script.js
+++ js/script.js
@@ -2,19 +2,18 @@
   var memoria = [10, 40, 40, 8, 20, 60, 100]; //Memórias para serem alocadas
   
   function first() {
-    var valordopacote = prompt("Qual o tamanho do pacote?")
-    result = valordopacote;
+    var valordoprocesso = prompt("Qual o tamanho do processo?")
+    result = valordoprocesso;
     const resultElement = document.getElementById("result");
-    resultElement.innerHTML = `Tamanho do pacote: ${result}MB`;
+    resultElement.innerHTML = `Tamanho do processo: ${result}MB`;
 
     let resultHTML = document.getElementById("result")
 
     for (let i=0; i<=6; i++){
-      if (valordopacote > memoria[i]){
-          resultHTML.innerHTML += `<p>A memória-${i} de tamanho ${memoria[i]}MB não alocou o pacote</p>`
-        
+      if (valordoprocesso > memoria[i]){
+          resultHTML.innerHTML += `<p>A memória-${i} de tamanho ${memoria[i]}MB não alocou o processo</p>`  
       }else{
-          resultHTML.innerHTML += `<p class="alocado">A memória-${i} de tamanho ${memoria[i]}MB conseguiu alocar o pacote</p>`
+          resultHTML.innerHTML += `<p class="alocado">A memória-${i} de tamanho ${memoria[i]}MB conseguiu alocar o processo</p>`
         i = 7
       }
     }    
