25df4b7fc839ca1c3f8a77dc345b408bb0a6af46
Aristides Jeronimo de Brito Neto <20231148060015@ifrn.local>
2023-07-24 16:21:17 -0300

    round-robin iniciado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
   let result;
-  var memoria = [10, 40, 40, 8, 20, 60, 100]; //Memórias para serem alocadas
+  var memoria = [10, 40, 40, 8, 20, 60, 100]; //Memórias para serem alocadas no first-fit e best-fit
   
   function first() {
     var valordoprocesso = prompt("Qual o tamanho do processo?")
@@ -22,8 +22,25 @@
     }    
   }
 
-  function round() {
+  function round() { //logica quase certa
+    var cpu = 220;
+    var pa = []; // PA == Process Array
+    var cp = []; // CP == CPU Processes
 
+    for (let i=0; i<=3; i++){
+      pa[i] = prompt(`Qual o ms da burst-time ${i+1}`)
+    }
+    let resultHTML = document.getElementById("result")
+
+    for (cpu; cpu != 0; cpu = cpu-10){
+      for (let p=0; p<=3; p++){
+        for (let r=0; r<=3; r++){
+          cp[r] = pa[p]-10
+          resultHTML.innerHTML += `<p class="p${p}">pa:${pa} cp: ${cp}</p>`
+        }
+      }
+    }
+    
   }
   
   function priority() {
